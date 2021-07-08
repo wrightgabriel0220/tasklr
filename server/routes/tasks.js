e5c322c849a88fabd5610a629712b677833dea80
Gabriel Wright <wrightgabriel0220@gmail.com>
2021-07-08 11:44:40 -0700

    Wrote GET route for tasks

---
+++ server/routes/tasks.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const db = require('./../../database/controllers.js');
+const tasks = express.Router();
+
+tasks.get('/list', (req, res) => {
+  db.getAllFrom('tasks')
+    .then(results => {
+      res.send(results);
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500);
+      res.end();
+    })
+});
+
+tasks.post('/add', (req, res) => {
+  // db.addSchedule(req.body.span, req.body.start, req.body.end)
+  //   .then(results => {
+  //     res.send(results);
+  //   })
+  //   .catch(err => {
+  //     console.error(err);
+  //     res.status(500);
+  //     res.end();
+  //   })
+});
+
+tasks.put('/:id/edit', (req, res) => {
+  // don't worry about this for current scope
+});
+
+tasks.delete('/:id/delete', (req, res) => {
+  // db.deleteSchedule(req.params.id)
+  //   .then(results => {
+  //     res.send(results);
+  //   })
+  //   .catch(err => {
+  //     console.error(err);
+  //     res.status(500);
+  //     res.end();
+  //   })
+});
+
+module.exports = schedules;
--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const schedules = require('./routes/schedules.js');
+const tasks = require('./routes/schedules.js');
 const db = require('./../database/controllers.js');
 
 const app = express();
@@ -10,6 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '../client/dist')));
 app.use('/schedules', schedules);
+app.use('/tasks', tasks);
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/dist/index.html'));
